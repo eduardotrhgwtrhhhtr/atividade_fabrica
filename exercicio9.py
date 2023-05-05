af95a2768b27bbab7a12cb0e1ee1ad0e901d3395
eduardotrhgwtrhhhtr <132004878+eduardotrhgwtrhhhtr@users.noreply.github.com>
2023-05-05 17:54:02 -0300

    exercicio9.py
    transformação de fahrenheit para celsius

---
+++ exercicio9.py
@@ -0,0 +1,5 @@
+fahrenheit = float(input("digite a temperatura de fajrenheit para celsius:\n"))
+celsius = (5/9)* (fahrenheit-32)
+print("transformação de fahrenheit para celsius é de: %.2f"%(celsius))
+
+#transformação de fahrenheit para celsius
