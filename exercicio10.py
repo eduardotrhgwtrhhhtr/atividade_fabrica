d6225af69bdb7df9a2b73eacd004af6a03cb7638
eduardotrhgwtrhhhtr <132004878+eduardotrhgwtrhhhtr@users.noreply.github.com>
2023-05-05 18:33:32 -0300

    aprimoramento_exercicio10.py

--- exercicio10.py
+++ exercicio10.py
@@ -1,10 +1,8 @@
-prod=input("digite o produto: ")
-quant=int(input ("digite a quantidade do produto: "))
-value=float(input("digite o valor do produto: "))
-per=float(input("de 1 a 100, digite qual o desconto do produto: "))
+nome = input("Qual o nome do seu produto? ")
+quantidade = int(input("Qual a quantidade que deseja comprar? "))
+valor = float(input("Qual o valor do produto? "))
+porc = float(input("Qual o valor do desconto? (em %) "))
+valor1 = (valor*quantidade)
+total = valor1-valor1 * (porc/100)
 
-
-totalprod=(value-(value*(per/100)))
-totalfinal=(totalprod*quant)
-
-print("o produto %s, na quantidade %d, vale %.2f"%(prod,quant,totalfinal))
+print ("Olá Sr(a). O produto %s de valor %.2f com a quantidade de %d e com desconto de %.2f custa no total: %s" % (nome,valor,quantidade,porc,total))
