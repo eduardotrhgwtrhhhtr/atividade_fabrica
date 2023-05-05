3d56dd92998dd64acc9d1080a53f4cd6ca185431
eduardotrhgwtrhhhtr <132004878+eduardotrhgwtrhhhtr@users.noreply.github.com>
2023-05-05 17:37:16 -0300

    updateexercicio2
    aprimorei

--- exercicio2.py
+++ exercicio2.py
@@ -2,7 +2,7 @@ valor = 5
 valor2 = 10
 valor3 = 3
 
-total= (valor+valor2) * valor3 
+total= (valor*valor2) * valor3 
 
-print ("O valor de",valor, "+", valor2, "=", total, sep=" ")
+print ("O valor de",valor, "*", valor2, "=", total, sep=" ")
 
