9659095a6ecb08af057b9dd4973e4b4cd91eecfd
eduardotrhgwtrhhhtr <132004878+eduardotrhgwtrhhhtr@users.noreply.github.com>
2023-05-10 17:26:01 -0300

    exercicio22.py

---
+++ exercicio22.py
@@ -0,0 +1,16 @@
+nome = input("qual o nome do aluno: ")
+série = input("qual a série: ")
+escola = input("qual a escola do aluno: ")
+cidade = input("qual a cidade: ")
+
+
+
+
+a=float(input("1 bimestre: "))
+b=float(input("2 bimestre: "))
+c=float(input("3 bimestre: "))
+d=float(input("4 bimestre :"))
+result=(a + b + c + d )/4
+print("o aluno: ",nome,"da série: ",série,"que estuda na escola: ",escola,"que mora na cidade: ",cidade,"foi com: ",result,"de média.")
+
+
