9b735699a1333c5beb84f2cf1bb8a64ed9fd14ef
eduardotrhgwtrhhhtr <132004878+eduardotrhgwtrhhhtr@users.noreply.github.com>
2023-05-17 17:24:55 -0300

    exercicio42.py

---
+++ exercicio42.py
@@ -0,0 +1,14 @@
+a=(input("digite um produto: "))
+b=(input("digite outro produto: "))
+c=(input("digite outro produto: "))
+
+
+
+a1=float(input("valor do primeiro produto: "))
+b2=float(input("valor do segundo produto: "))
+c3=float(input("valor do terceiro produto: "))
+
+barato=[a1,b2,c3]
+print(min(barato))
+barato1=[a,b,c]
+print(min(barato1))
