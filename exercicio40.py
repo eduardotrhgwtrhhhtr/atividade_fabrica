ea000c2e06a75dc9660506ef414b39111fe93bf9
eduardotrhgwtrhhhtr <132004878+eduardotrhgwtrhhhtr@users.noreply.github.com>
2023-05-17 16:16:29 -0300

    exercicio40.py

---
+++ exercicio40.py
@@ -0,0 +1,20 @@
+a=float(input("digite um numero: "))
+b=float(input("digite outro numero: "))
+c=float(input("digite outro numero: "))
+lista=[a,b,c]
+print(max(lista))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
