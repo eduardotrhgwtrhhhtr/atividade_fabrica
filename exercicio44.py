876666b8b57a7a7ac382ea949381b81886aefe02
eduardotrhgwtrhhhtr <132004878+eduardotrhgwtrhhhtr@users.noreply.github.com>
2023-05-17 18:35:54 -0300

    exercicio44.py

---
+++ exercicio44.py
@@ -0,0 +1,10 @@
+turno=input("qual turno estuda: (M-V-N)")
+turno = turno.upper()
+if turno=="M":
+    print("bom dia!")
+elif turno=="V":
+    print("boa tarde!")
+elif turno=="N":
+    print("boa noite!")
+else:
+    print("nada encontrado.")
