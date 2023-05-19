3468b7406bf1b7b9a5e3bf6f3d6fffb0b4c722af
eduardotrhgwtrhhhtr <132004878+eduardotrhgwtrhhhtr@users.noreply.github.com>
2023-05-19 15:30:34 -0300

    updateexercicio45.py

--- exercicio45.py
+++ exercicio45.py
@@ -1,11 +1,8 @@
-print("-----------------BEM VINDO----------------")
-print("Quer saber se seu numero quadrado é perfeito? ")
-
-a=float(input("numero: "))
-b=float(input("numero: "))
-
-if a==b :
-    print("O quadrado é perfeito!")
-else:
-    print("O quadrado é imperfeito!")
-print("Obrigado!!!!!")
+print("-----------------BEM VINDO----------------") # bem vindo
+a=float(input("numero: ")) # pedindo o numero a
+b=float(input("numero: ")) # pedindo o numero b
+if a==b :  # se o numero a, for igual ao numero b
+    print("O quadrado é perfeito!") # vai ler que o quadrado é perfeito
+else: # se nao for
+    print("O quadrado é imperfeito!") # vai ler que o quadrado é imperfeito
+print("Obrigado!!!!!") #obrigado
---
+++ exercicio46.py
@@ -0,0 +1,7 @@
+while True:
+    valor=int(input("digite 1 ou 0 para encerrar: "))
+    if valor==1:
+        print("valor correto")
+    else:
+        print("valor para sair")
+        break
