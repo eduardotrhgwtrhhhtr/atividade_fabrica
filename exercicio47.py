9294d162e02031b8d4cdb277002f27988f5a8efd
eduardotrhgwtrhhhtr <132004878+eduardotrhgwtrhhhtr@users.noreply.github.com>
2023-05-19 15:59:45 -0300

    exercicio47.py
    para reduzir o numero até 0

---
+++ exercicio47.py
@@ -0,0 +1,8 @@
+a=int(input("me diga um numero para reduzi-lo até 0: "))
+a+= 1                            
+while True:
+    a-=1
+    print(a)
+    if a == 0:
+        break
+#para reduzir o numero até 0
