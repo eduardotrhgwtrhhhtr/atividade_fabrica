d41511db34517dfe32e3c9e1e8eabde40cb958c0
eduardotrhgwtrhhhtr <132004878+eduardotrhgwtrhhhtr@users.noreply.github.com>
2023-05-19 15:09:43 -0300

    exerciciotrabalho.py

---
+++ exercicio45.py
@@ -0,0 +1,11 @@
+print("-----------------BEM VINDO----------------")
+print("Quer saber se seu numero quadrado é perfeito? ")
+
+a=float(input("numero: "))
+b=float(input("numero: "))
+
+if a==b :
+    print("O quadrado é perfeito!")
+else:
+    print("O quadrado é imperfeito!")
+print("Obrigado!!!!!")
