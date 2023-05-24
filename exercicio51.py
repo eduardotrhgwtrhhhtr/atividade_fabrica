9655f7fccb0bbd54d8c20058deeb5740defcd801
eduardo <eduardojheron5@gmail.com>
2023-05-24 18:20:23 -0300

    exercicio51.py

---
+++ exercicio51.py
@@ -0,0 +1,9 @@
+#tabuada
+for x in range (1,11):
+    print("="*20)
+    for x2 in range(1,11):
+        total = x*x2
+        print(x, "x", x2, "=",total)
+#contagem até 100 com numeros pares
+for a in range(0,102,2):
+    print(a)
