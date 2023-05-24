7b4034f8b3ddbd76bd04902e1778da8c56956f61
eduardo <eduardojheron5@gmail.com>
2023-05-24 17:25:31 -0300

    updateexerciciop49.py

--- exercicio45.py
+++ exercicio45.py
@@ -1,13 +1,13 @@
 # verificar se o numero quadrado é perfeito
-print("-----------------BEM VINDO----------------") # bem vindo
+print("-----------------BEM VINDO----------------") 
 a=float(input("digite o primeiro lado do quadrado: ")) # pedindo o primeiro lado do quadrado
 b=float(input("digite o segundo lado do quadrado: ")) # pedindo o segundo lado do quadrado
-c=float(input("digite o terceiro lado do quadrado: "))#pedindo o terceiro lado do quadrado
-d=float(input("digite o quarto lado do quadrado: ")) #pedindo o quarto lado do quadrado
+c=float(input("digite o terceiro lado do quadrado: ")) # pedindo o terceiro lado do quadrado
+d=float(input("digite o quarto lado do quadrado: ")) # pedindo o quarto lado do quadrado
 if a==b==c==d :  # se todos os numeros forem iguais
     print("O quadrado é perfeito!") # vai ler que o quadrado é perfeito
 else: # se nao for
     print("O quadrado é imperfeito!") # vai ler que o quadrado é imperfeito
-print("Obrigado!!!!!") #obrigado
+print("Obrigado!!!!!") 
 
 
