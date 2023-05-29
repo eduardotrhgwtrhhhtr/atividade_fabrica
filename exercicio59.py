e1a336d3cc8bca9133e35ca3aa7f85271f12a42a
eduardo <eduardojheron5@gmail.com>
2023-05-29 16:58:11 -0300

    exercicio59.py

---
+++ exercicio59.py
@@ -0,0 +1,6 @@
+#crie uma lista vazia que será usada para fazer uma lista de supermercados,preencha a lista com 10 itens e imprima o resultado
+mercado=[]
+for a in range(11):
+    mercado.append(input("digite 10 itens: "))
+for a in mercado:
+        print(a)
