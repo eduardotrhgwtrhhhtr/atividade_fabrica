8079207908ad3a0ace4c82b3bb954d94b4b63d21
eduardo <eduardojheron5@gmail.com>
2023-05-29 16:11:16 -0300

    exercicio57.py

---
+++ exercicio57.py
@@ -0,0 +1,5 @@
+#crie uma lista com 10 nomes e exiba-os no terminal de repetiçao usando o for
+lista=["gabriel","chris","vinicius","fofin","gleissin","eduardo","graveto","mauricio","ederson",]
+for x in lista:
+    print(x)
+
---
+++ exercicio58.py
@@ -0,0 +1,5 @@
+
+#crie uma lista com 10 numeros e exiba a soma dos numeros
+num=['1','2','3','4','5','6','7','8','9','10']
+for a in range(num)+1:
+    print(a)
