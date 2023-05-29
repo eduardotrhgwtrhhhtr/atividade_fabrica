382f8700c42792d60b1eaa42869d4bdb693630d3
eduardo <eduardojheron5@gmail.com>
2023-05-29 16:22:56 -0300

    update_exercicio58.py

--- exercicio58.py
+++ exercicio58.py
@@ -1,5 +1,5 @@
-
 #crie uma lista com 10 numeros e exiba a soma dos numeros
-num=['1','2','3','4','5','6','7','8','9','10']
-for a in range(num)+1:
+list=[1,2,3,4,5,6,7,8,9,10]
+for a in list:
     print(a)
+print(sum(list))
