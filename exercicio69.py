71e263596f2cfa4501fbe353ae55dabee7579e05
eduardo <eduardojheron5@gmail.com>
2023-06-07 17:52:23 -0300

    exercicio69.py

--- exercicio69.py
+++ exercicio69.py
@@ -5,7 +5,10 @@ while True:
         print("2- CADASTRO PASSAGEM")
         print("3- CADASTRO AVIAO")
         print("4- CADASTRO TRIPULAÇAO")
-        print("5- INPRIMIR RELATORIO")
+        print("5- IMPRIMIR RELATORIO DO CLIENTE")
+        print("6- IMPRIMIR RELATORIO DO CADASTRO PASSAGEM")
+        print("7- IMPRIMIR RELATORIO DO CADASTRO DO AVIAO")
+        print("8- IMPRIMIR RELATORIO DO CADASTRO DA TRIPULAÇAO")
         print("0- SAIR")
         opcoes=int(input("QUAL SUA ESCOLHA? "))
     except ValueError:
@@ -27,6 +30,7 @@ while True:
             except ValueError:
                 print("tente novamente... digite somente numeros.")
             else:
+                #listanome.append(nome)
                 break
     if opcoes==2:
         while True:
@@ -71,32 +75,39 @@ while True:
                 print("tente novamente... digite somente numeros inteiros.")
             else:
                 break
-
     if opcoes==5:
-        print(nome)
-        print(sobrenome)
-        print(fone)
-        print(idade)
-        print(rg)
-        print(cpf) 
-        print("=================")
-        print(destino)
-        print(origem)
-        print(duraçao)
-        print(valorpass)
-        print(desconto)
-        print(result)
-        print("=============")
-        print(ano)
-        print(horasdevoo)
-        print(cor)
-        print(qntdp)
-        print(modelo)
-        print("=============")
-        print(nome2)
-        print(cargo)
-        print(idade2)
-        print(datae)
-        print(fone2)
+        print("nome: ", nome)
+        print("sobrenome: ", sobrenome)
+        print("fone: ",fone)
+        print("idade",idade)
+        print("rg: ",rg)
+        print("cpf: ",cpf)
+        print("PROGRAMA FINALIZADO.")
+        break
+    if opcoes==6:
+        print("destino: ",destino)
+        print("origem: ",origem)
+        print("duraçao: ",duraçao)
+        print("valor da passagem: ",valorpass)
+        print("desconto: ",desconto)
+        print("resultado: ",result)
+        print("PROGRAMA FINALIZADO.")
+        break
+    if opcoes==7:    
+        print("ano: ",ano)
+        print("horas de voo: ",horasdevoo)
+        print("cor: ",cor)
+        print("quantidade de passageiros: ",qntdp)
+        print("modelo: ",modelo)
+        print("PROGRAMA FINALIZADO.")
+        break
+    if opcoes==8:
+        print("nome: ",nome2)
+        print("cargo: ",cargo)
+        print("idade: ",idade2)
+        print("data de admissao: ",datae)
+        print("fone: ",fone2)
+        break
     if opcoes==0:
+        print("programa encerrado.")
         break
