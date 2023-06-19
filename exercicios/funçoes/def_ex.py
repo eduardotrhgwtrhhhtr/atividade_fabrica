9c420fe094157597437ef3e793c302d962dcfb25
eduardo <eduardojheron5@gmail.com>
2023-06-19 17:51:39 -0300

    def_ex.py

---
+++ exercicios/funçoes/def_ex.py
@@ -0,0 +1,9 @@
+#tres argumentos
+def soma(nome,sobrenome,idade):
+    print("nome: ",nome)
+    print("sobrenome: ",sobrenome)
+    print("voce tem",idade,"anos")
+a=input("qual seu nome: ")
+b=input("qual seu sobrenome: ")
+c=int(input("qual sua idade: "))
+soma(a,b,c)
