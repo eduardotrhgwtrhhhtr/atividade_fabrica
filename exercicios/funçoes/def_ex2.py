ec77fdccaa84aa91c70acd7bed1838478fd82fe7
eduardo <eduardojheron5@gmail.com>
2023-06-19 18:19:03 -0300

    def_ex2.py

---
+++ exercicios/funçoes/def_ex2.py
@@ -0,0 +1,9 @@
+def soma(a):
+    if a<0:
+        print("negativo!")
+    elif a>0:
+        print("positivo!")
+    elif a==0: 
+        print("ZEROO")
+a=float(input("digite um numero: "))
+soma(a)
