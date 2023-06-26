0be945bb1e4c79cec1770eefeaf50d6027b4ea85
eduardo <eduardojheron5@gmail.com>
2023-06-26 15:55:47 -0300

    import.datetime.py

--- bibliotecas.py/import.datetime.py
+++ bibliotecas.py/import.datetime.py
@@ -1,4 +1,12 @@
 import datetime as dt
 #mostrar a data
 print(dt.date.today()) 
-#
+#formatando datas
+print(dt.date.today().strftime("%d/%m/%y"))
+#mostra as horas
+print(dt.datetime.now())
+#data e hora
+import os
+while True:
+    print(dt.datetime.now().strftime("%d/%M/%Y/%H:%M"))
+    os.system("cls")#limpa a tela
