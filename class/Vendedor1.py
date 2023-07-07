93cf3d38e41f2ebf60bd5be10b99c51f99caedfa
eduardo <eduardojheron5@gmail.com>
2023-07-07 17:01:00 -0300

    VendedorTeste.py

--- class/Vendedor1.py
+++ class/Vendedor1.py
@@ -2,3 +2,13 @@ class Vendedor():
     def __init__(self,nome,vendas):
         self.nome = nome
         self.vendas = vendas
+    def vendeu(self,vendas):
+        self.vendas=vendas
+        self.vendas
+        print(self.vendas)
+    def bateu_meta(self,meta):
+        if self.vendas>=meta:
+            print("bateu a meta.")
+        else:
+            print("nao bateu a meta.")
+
--- class/VendedorTeste.py
+++ class/VendedorTeste.py
@@ -1,11 +1,6 @@
 from Vendedor1 import *
-vendedor1 = Vendedor("Ederson",1000)
-print (vendedor1.nome)
-##############################################################################
 
-a = input("Nome: ")
-b = int(input("Valor: "))
 
-vendedor2 = Vendedor(a,b)
-print (vendedor2.nome)
-print (vendedor2.vendas)
+vendedor1=Vendedor('ederson',1000)
+print(vendedor1.nome)
+vendedor1.bateu_meta(100)
