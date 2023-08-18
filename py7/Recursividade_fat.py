36f38ab29743c76fab43d2a6234f19e7d1981c08
eduardo <eduardojheron5@gmail.com>
2023-08-18 15:41:27 -0300

    a
    a

--- py7/Recursividade_fat.py
+++ py7/Recursividade_fat.py
@@ -1,7 +1,6 @@
+numero = int(input("Digite o numero para calcular o Fatorial: ") )
 resultado=1
 count=1
-numero = int(input("Digite o numero para calcular o Fatorial: ") )
-
 while count <= numero:
     resultado *= count
     count += 1
