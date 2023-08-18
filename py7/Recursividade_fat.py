79780619e8a0aba3f4dee82093ec65a1872df434
eduardo <eduardojheron5@gmail.com>
2023-08-18 15:45:38 -0300

    a
    a

--- py7/Recursividade_fat.py
+++ py7/Recursividade_fat.py
@@ -1,9 +1,19 @@
-numero = int(input("Digite o numero para calcular o Fatorial: ") )
+'''numero = int(input("Digite o numero para calcular o Fatorial: ") )
 resultado=1
 count=1
 while count <= numero:
     resultado *= count
     count += 1
-print(resultado)
+print(resultado)'''
 
 #FATORAÇAO
+
+def soma(n):
+    if n == 1:
+        return 1
+    else:
+        return n * soma (n - 1)
+
+numero = 5
+result = soma(numero)
+print(f"o fatorial do {numero} é {result}")
