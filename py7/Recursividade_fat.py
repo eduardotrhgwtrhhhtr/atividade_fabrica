ad08d2a7ad89ba95f96ee016600af4ea06ffb235
eduardo <eduardojheron5@gmail.com>
2023-08-18 17:09:08 -0300

    a
    a

--- py7/Recursividade_fat.py
+++ py7/Recursividade_fat.py
@@ -4,7 +4,7 @@ count=1
 while count <= numero:
     resultado *= count
     count += 1
-print(resultado)'''
+print(resultado)
 
 #FATORAÇAO
 
@@ -16,4 +16,22 @@ def soma(n):
 
 numero = 5
 result = soma(numero)
-print(f"o fatorial do {numero} é {result}")
+print(f"o fatorial do {numero} é {result}")'''
+
+#torre de hanoi
+
+def TorreDeHanoi(n, origem, destino, auxiliar):
+    if n == 1:
+        print(f"move o disco 1 de {origem} a {destino}")
+        return 1
+    
+    movimentos = 0
+    movimentos += TorreDeHanoi(n - 1, origem, auxiliar, destino )
+    print(f"move o disco {n} de {origem} a {destino}")
+    movimentos += 1
+    movimentos += TorreDeHanoi(n-1, auxiliar, destino,  origem)
+    return movimentos 
+
+num=3
+resultMovim=TorreDeHanoi(num,'A','C','B')
+print(f'total de movmentos necessarios: {resultMovim}')
