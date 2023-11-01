e5a92a1af952a6a327c80e7a558fb581002020ee
eduardo <eduardojheron5@gmail.com>
2023-11-01 14:57:14 -0300

    a
    a

--- bancodados_HubInnovation.sql
+++ bancodados_HubInnovation.sql
@@ -13,10 +13,7 @@ id_palestra int not null,
 constraint foreign key (id_palestra) references palestra(id_palestra)
 );
 
-describe usuario;
-alter table usuario modify column data_nasc varchar(30);
-drop table palestra;
-drop table usuario;
+
 
 create table palestra (
 id_palestra int auto_increment key,
@@ -28,15 +25,30 @@ nome_palestrante varchar(40)not null,
 vagas int
 );
 
-describe usuario;
+
 
 insert into usuario  (nome,email,fone,cpf,data_nasc,sexo,id_palestra)values
-('dieguin','@gmail','999','999','2000-11-01','mascu',1)
+('dieguin','@gmail','999','999','2000-11-01','feminino',1),
+('pontes','@gmail','999','999','2000-11-01','mascu',2),
+('chris','@gmail','999','999','2000-11-01','mascu',3),
+('gleiton','@gmail','999','999','2000-11-01','mascu',4),
+('graveto','@gmail','999','999','2000-11-01','mascu',5),
+('vinicin','@gmail','999','999','2000-11-01','mascu',6),
+('fofinho','@gmail','999','999','2000-11-01','mascu',7)
 ;
+
+
+insert into palestra  (desc_palestra,nome_palestra,id_sala,id_palestrante,nome_palestrante,vagas) values
+('ssssssss','palestra',1,1,'dieguin',1),
+('dddddddd','palestra',2,2,'pontes',2),
+('ffffffff','palestra',3,3,'chris',3),
+('gggggg','palestra',4,4,'gleiton',4),
+('hhhhhhhh','palestra',5,5,'graveto',5),
+('jjjjjjjjjjj','palestra',6,6,'vinicin',6),
+('kkkkkkkkkkk','palestra',7,7,'fofinho',7)
+;
+
+describe usuario;
+describe palestra;
 select * from palestra;
 select * from usuario;
-insert into palestra  (desc_palestra,nome_palestra,id_sala,id_palestrante,nome_palestrante,vagas) values
-('fjjhg','palestra',1,1,'dieguin',1);
-
-
-
