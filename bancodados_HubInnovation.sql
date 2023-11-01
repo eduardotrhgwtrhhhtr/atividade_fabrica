20ec8c4d67898ace700fb4b99bf2e5fa68352639
eduardo <eduardojheron5@gmail.com>
2023-11-01 14:57:31 -0300

    a
    a

--- bancodados_HubInnovation.sql
+++ bancodados_HubInnovation.sql
@@ -1,6 +1,17 @@
 create database HubInnovation;
 use HubInnovation;
 
+create table palestra (
+id_palestra int auto_increment key,
+desc_palestra text(200)not null,
+nome_palestra varchar(40)not null,
+id_sala int not null ,
+id_palestrante int not null,
+nome_palestrante varchar(40)not null,
+vagas int
+);
+
+
 create table usuario(
 id_usuario int auto_increment primary key,
 nome varchar(30) not null,
@@ -14,17 +25,15 @@ constraint foreign key (id_palestra) references palestra(id_palestra)
 );
 
 
-
-create table palestra (
-id_palestra int auto_increment key,
-desc_palestra text(200)not null,
-nome_palestra varchar(40)not null,
-id_sala int not null ,
-id_palestrante int not null,
-nome_palestrante varchar(40)not null,
-vagas int
-);
-
+insert into palestra  (desc_palestra,nome_palestra,id_sala,id_palestrante,nome_palestrante,vagas) values
+('ssssssss','palestra',1,1,'dieguin',1),
+('dddddddd','palestra',2,2,'pontes',2),
+('ffffffff','palestra',3,3,'chris',3),
+('gggggg','palestra',4,4,'gleiton',4),
+('hhhhhhhh','palestra',5,5,'graveto',5),
+('jjjjjjjjjjj','palestra',6,6,'vinicin',6),
+('kkkkkkkkkkk','palestra',7,7,'fofinho',7)
+;
 
 
 insert into usuario  (nome,email,fone,cpf,data_nasc,sexo,id_palestra)values
@@ -37,16 +46,6 @@ insert into usuario  (nome,email,fone,cpf,data_nasc,sexo,id_palestra)values
 ('fofinho','@gmail','999','999','2000-11-01','mascu',7)
 ;
 
-
-insert into palestra  (desc_palestra,nome_palestra,id_sala,id_palestrante,nome_palestrante,vagas) values
-('ssssssss','palestra',1,1,'dieguin',1),
-('dddddddd','palestra',2,2,'pontes',2),
-('ffffffff','palestra',3,3,'chris',3),
-('gggggg','palestra',4,4,'gleiton',4),
-('hhhhhhhh','palestra',5,5,'graveto',5),
-('jjjjjjjjjjj','palestra',6,6,'vinicin',6),
-('kkkkkkkkkkk','palestra',7,7,'fofinho',7)
-;
 
 describe usuario;
 describe palestra;
