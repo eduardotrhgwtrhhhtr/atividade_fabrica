905a53525273d559760a7498a8b0ebbdd7e123e8
eduardo <eduardojheron5@gmail.com>
2023-11-06 17:37:45 -0300

    a
    a

---
+++ KivyMd/Kivymd.py
@@ -0,0 +1,30 @@
+from kivy.lang import Builder
+
+from kivymd.app import MDApp
+from kivymd.uix.pickers import MDTimePicker
+
+KV = '''
+MDFloatLayout:
+
+    MDRaisedButton:
+        text: "Open time picker"
+        pos_hint: {'center_x': .5, 'center_y': .5}
+        on_release: app.show_time_picker()
+'''
+
+
+class Test(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        return Builder.load_string(KV)
+
+    def show_time_picker(self):
+        '''Open time picker dialog.'''
+
+        time_dialog = MDTimePicker()
+        time_dialog.open()
+
+
+Test().run()
+#relogio zika
